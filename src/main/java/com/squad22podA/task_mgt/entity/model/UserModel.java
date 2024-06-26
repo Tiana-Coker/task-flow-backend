fb129a62100e9cc47640dc39a6a177750629b07e
Chinecherem <chinecheremubawike@gmail.com>
2024-06-26 12:44:21 +0100

    Added ApplicationConfig file and Modified UserModel to implement UserDetails and add Role

--- src/main/java/com/squad22podA/task_mgt/entity/model/UserModel.java
+++ src/main/java/com/squad22podA/task_mgt/entity/model/UserModel.java
@@ -1,12 +1,17 @@
 package com.squad22podA.task_mgt.entity.model;
 
 
+import com.squad22podA.task_mgt.entity.enums.Role;
 import jakarta.persistence.*;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import lombok.*;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 
 import java.util.ArrayList;
+import java.util.Collection;
 import java.util.List;
 
 @Entity
@@ -16,7 +21,7 @@ import java.util.List;
 @AllArgsConstructor
 @NoArgsConstructor
 @Builder
-public class UserModel extends BaseClass{
+public class UserModel extends BaseClass implements UserDetails {
 
     @NotBlank(message = "First name is required")
     private String firstName;
@@ -37,9 +42,45 @@ public class UserModel extends BaseClass{
 
     private String phoneNumber;
 
+    private Role role;
+
     private boolean enabled = false;
 
     @OneToMany(mappedBy = "userModel", cascade = CascadeType.ALL)
     private List<Task> taskList = new ArrayList<>();
 
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of(new SimpleGrantedAuthority(role.name()));
+    }
+
+    @Override
+    public String getPassword() {
+        return password;
+    }
+
+    @Override
+    public String getUsername() {
+        return email;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
 }
